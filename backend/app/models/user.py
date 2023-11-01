4d04f615098536eae5b8f07d8b3a829c65d6c756
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-11-01 13:05:16 +0200

    Update user.py  to add the username in the db (128 char)

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,11 +5,13 @@ class User(BaseModel):
     """
     Represents a user of the flashcard application.
     """
+    username = db.StringField(unique=True, max_length=128, required=True)  # Added username field
     email = db.EmailField(unique=True, required=True)
     password = db.StringField(required=True)
     first_name = db.StringField(max_length=128, required=True)
     last_name = db.StringField(max_length=128, required=True)
 
     def __repr__(self):
-        return f"<User(email={self.email}, first_name={self.first_name}, last_name={self.last_name})>"
+        return f"<User(username={self.username}, email={self.email}, first_name={self.first_name}, last_name={self.last_name})>"
+
 
