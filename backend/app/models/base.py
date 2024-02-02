eb92b96358cd500243470987ce128f4e517a37ff
steve2700 <stevnyaru@github.com>
2024-02-02 21:28:27 +0200

    users can now create and login to their accounts

--- backend/app/models/base.py
+++
@@ -1,9 +0,0 @@
-from backend.app import db
-
-class BaseModel(db.Document):
-    created_at = db.DateTimeField(default=datetime.datetime.utcnow)
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}(id={self.id}, created_at={self.created_at})>"
-
-
