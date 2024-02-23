7ee114577e383127381b874372c8b7057a2c0d9d
Elizabeth-Panashe-Pfachi <testlolabtest@gmail.com>
2024-02-23 11:38:51 +0200

    Ädd basic logic for the home page

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+//import ax
+
+const Home = () => {
+    // when the page loads send a requetst to api to fetch user flashcard category
+    // display the categories as folders.
+    // When the folders are clicked they will go their
+    // respective folder page and display the flasscards there
+    // this will have its own component/page jsx
+    // display the categories using map
+
+    // const url = apiUrlToFetchCategories
+    const homeData = () => {
+        // fetch the data from the api
+        // try {
+        //     const gresponse = await axios.get(url)
+        //     displayData = gresponse.data
+        // } catch (error) {
+        //     console.log(error.response)
+        // }
+    }
+    return (
+        <section>
+            (displayData.map(
+    (data) => {
+                <article> {data.name}</article>
+            }
+            ))
+        </section>
+    )
+}
