4dd7581a3d06b5c8e25f41f0a66c9e158b5c8752
Elizabeth-Panashe-Pfachi <testlolabtest@gmail.com>
2024-03-06 15:32:14 +0200

    Match fields in the frontend with fields in the backend

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import ToggleButton from '../components/toggleButton';
 
+/*
+So the email that we will send to the user contains the login link?
+
+*/
+
 const Signup = () => {
     const { register, handleSubmit, formState: { errors }, watch } = useForm();
 
-    const SignUp = (data) => {
-        console.log(`Signing up: ${data}`);
+    const SignUp = async (data) => {
+        // const signingUp = {
+        //     userName: data.userName,
+        //     email: data.email,
+        //     password: data.password,
+        // }
+
+        // const response = await axios.post('http://localhost:3003/auth/signup', signingUp);
+        console.log(data);
+
     }
 
     return (
         <section className='display'>
             <ToggleButton />
+            <h1 className='text-xl font-semibold'>Create your Free Account</h1>
             <form className='display' onSubmit={handleSubmit(SignUp)}>
-                <label htmlFor="fullName" className='formLabel'>Full Name:</label>
-                <input type="text" id="fullName" className='formInput'
-                    {...register('fullName',
+                <label htmlFor="userName" className='formLabel'>User Name:</label>
+                <input type="text" id="userName" className='rounded'
+                    {...register('userName',
                         {
                             required: {
                                 value: true,
@@ -47,8 +61,19 @@ const Signup = () => {
                                 value: 8,
                                 message: "Password must be at least 8 characters",
                             },
+                            pattern: {
+                                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                message: `Password must contain at least: one uppercase letter, one lowercase letter, one number and one special character`,
+                            },
                         })}
                 />
+                {errors.password && (
+                    <ul className="text-xs text-rose-700 text-left">
+                        {errors.password.message.split(',').map((message, index) => (
+                            <li key={index}>   {message}</li>
+                        ))}
+                    </ul>
+                )}
 
                 <label htmlFor="confirmation" className='formLabel'>Confirm Password:</label>
                 <input
@@ -70,11 +95,9 @@ const Signup = () => {
                     <span className="errorMessage">{errors.confirmation.message}</span>
                 )}
 
-                <label htmlFor="grade" className='formLabel'>Grade:</label>
-                <input type="text" id="grade" className='formInput' />
-
                 <button type="submit" className='formButon'>Sign Up</button>
             </form>
+            <p>Or Sign up with Google</p>
         </section>
     );
 };
